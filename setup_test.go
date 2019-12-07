fafb4cdd76d389517df64499f4e27d60834b2756
milgradesec <danielpaesa@outlook.com>
2019-12-07 11:58:49 +0100

    test improved

--- setup_test.go
+++ setup_test.go
@@ -12,6 +12,32 @@ func TestSetup(t *testing.T) {
 		t.Fatalf("Expected errors, but got: %v", err)
 	}
 
+	c = caddy.NewTestController("dns", `filter more`)
+	if err := setup(c); err == nil {
+		t.Fatalf("Expected errors, but got: %v", err)
+	}
+
+	c = caddy.NewTestController("dns", `filter {
+		more
+		}`)
+	if err := setup(c); err == nil {
+		t.Fatalf("Expected errors, but got: %v", err)
+	}
+
+	c = caddy.NewTestController("dns", `filter {
+		block url more
+		}`)
+	if err := setup(c); err == nil {
+		t.Fatalf("Expected errors, but got: %v", err)
+	}
+
+	c = caddy.NewTestController("dns", `filter {
+		allow path more
+		}`)
+	if err := setup(c); err == nil {
+		t.Fatalf("Expected errors, but got: %v", err)
+	}
+
 	c = caddy.NewTestController("dns", `filter {
 		allow https://dl.paesacybersecurity.eu/lists/whitelist.txt
 		block https://dl.paesacybersecurity.eu/lists/blacklist.txt
