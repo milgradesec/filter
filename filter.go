a8f2e474104e8246e660acfd3555e81a99453bdf
milgradesec <38557680+milgradesec@users.noreply.github.com>
2020-02-04 05:47:20 +0100

    remove reload.go

--- filter.go
+++ filter.go
@@ -45,6 +45,32 @@ func (f *filter) Match(str string) bool {
 	return false
 }
 
+func (f *filter) Load() error {
+	whitelist := NewPatternMatcher()
+	blocklist := NewPatternMatcher()
+
+	for _, list := range f.Lists {
+		rc, err := list.Open()
+		if err != nil {
+			return err
+		}
+		if list.Block {
+			if _, err := blocklist.ReadFrom(rc); err != nil {
+				return err
+			}
+		} else {
+			if _, err := whitelist.ReadFrom(rc); err != nil {
+				return err
+			}
+		}
+		rc.Close()
+	}
+
+	f.whitelist = whitelist
+	f.blacklist = blocklist
+	return nil
+}
+
 func (f *filter) OnStartup() error {
 	return f.Load()
 }
