3ccc6540b74a3f898bc5a6aec23d10aa47b549d5
milgradesec <38557680+milgradesec@users.noreply.github.com>
2020-02-16 19:01:26 +0100

    improve pattern matcher

--- list.go
+++ list.go
@@ -1,11 +1,8 @@
 package filter
 
 import (
-	"bufio"
-	"errors"
 	"io"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -35,101 +32,4 @@ func (l *List) Open() (src io.ReadCloser, err error) {
 		src = file
 	}
 	return src, nil
-}
-
-type PatternMatcher struct {
-	hashtable  map[string]struct{}
-	prefixes   []string
-	suffixes   []string
-	subStrings []string
-	regexes    []*regexp.Regexp
-}
-
-func NewPatternMatcher() *PatternMatcher {
-	return &PatternMatcher{
-		hashtable: make(map[string]struct{}),
-	}
-}
-
-var regexpRunes = []string{"[", "]", "(", ")", "|", "?",
-	"+", "$", "{", "}", "^"}
-
-func (l *PatternMatcher) ReadFrom(r io.Reader) (n int64, err error) {
-	if r == nil {
-		return 0, errors.New("invalid list source")
-	}
-
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			continue
-		}
-		if strings.HasPrefix(line, "#") {
-			continue
-		}
-		for _, op := range regexpRunes {
-			if strings.Contains(line, op) {
-				r, err := regexp.Compile(line)
-				if err != nil {
-					return 0, err
-				}
-				l.regexes = append(l.regexes, r)
-				break
-			}
-		}
-		if strings.Contains(line, "*") {
-			if strings.HasSuffix(line, "*") && strings.HasPrefix(line, "*") {
-				qname := strings.TrimPrefix(line, "*")
-				qname = strings.TrimSuffix(qname, "*")
-				l.subStrings = append(l.subStrings, qname)
-			}
-			if strings.HasSuffix(scanner.Text(), "*") {
-				domain := strings.TrimSuffix(line, "*")
-				l.prefixes = append(l.prefixes, domain)
-			}
-			if strings.HasPrefix(scanner.Text(), "*") {
-				domain := strings.TrimPrefix(line, "*")
-				l.suffixes = append(l.suffixes, domain)
-			}
-		} else {
-			l.hashtable[line] = struct{}{}
-		}
-
-		if scanner.Err() != nil {
-			return 0, scanner.Err()
-		}
-	}
-	return n, nil
-}
-
-func (l *PatternMatcher) Match(str string) bool {
-	_, q := l.hashtable[str]
-	if q {
-		return true
-	}
-	for _, prefix := range l.prefixes {
-		if strings.HasPrefix(str, prefix) {
-			return true
-		}
-	}
-	for _, suffix := range l.suffixes {
-		if strings.HasSuffix(str, suffix) {
-			return true
-		}
-		if str == strings.TrimPrefix(suffix, ".") {
-			return true
-		}
-	}
-	for _, substr := range l.subStrings {
-		if strings.Contains(str, substr) {
-			return true
-		}
-	}
-	for _, regex := range l.regexes {
-		if regex.MatchString(str) {
-			return true
-		}
-	}
-	return false
 }
